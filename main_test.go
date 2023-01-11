de0a451b792c9cbf6d4c735b67ab2f441214d2e8
thaim <thaim24@gmail.com>
2023-01-11 00:42:17 +0000

    apply fmt

--- main_test.go
+++ main_test.go
@@ -7,34 +7,34 @@ import (
 
 func TestMainPrintIds(t *testing.T) {
 	cases := []struct {
-		name string
-		ids []string
+		name    string
+		ids     []string
 		allFlag bool
-		expect string
+		expect  string
 	}{
 		{
-			name: "print nothing with empty id",
-			ids: nil,
+			name:    "print nothing with empty id",
+			ids:     nil,
 			allFlag: false,
-			expect: "",
+			expect:  "",
 		},
 		{
-			name: "print single id with single id",
-			ids: []string{"i-012345"},
+			name:    "print single id with single id",
+			ids:     []string{"i-012345"},
 			allFlag: false,
-			expect: "i-012345\n",
+			expect:  "i-012345\n",
 		},
 		{
-			name: "print single id with multiple ids and disabled all flag",
-			ids: []string{"i-012345", "i-6789ab", "i-cdef01"},
+			name:    "print single id with multiple ids and disabled all flag",
+			ids:     []string{"i-012345", "i-6789ab", "i-cdef01"},
 			allFlag: false,
-			expect: "i-012345\n",
+			expect:  "i-012345\n",
 		},
 		{
-			name: "print multiple ids with multiple ids and enabled all flag",
-			ids: []string{"i-012345", "i-6789ab", "i-cdef01"},
+			name:    "print multiple ids with multiple ids and enabled all flag",
+			ids:     []string{"i-012345", "i-6789ab", "i-cdef01"},
 			allFlag: true,
-			expect: "i-012345\ni-6789ab\ni-cdef01\n",
+			expect:  "i-012345\ni-6789ab\ni-cdef01\n",
 		},
 	}
 
